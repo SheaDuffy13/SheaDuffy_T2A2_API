5245f73e1b4632a3bdacbb5eff3a1be4ca2c5ab6
SheaDuffy13 <13420@coderacademy.edu.au>
2022-11-02 01:18:29 +1000

    added cli commands

---
+++ src/controllers/cli_controller.py
@@ -0,0 +1,91 @@
+from flask import Blueprint
+from init import db, bcrypt
+from models.book import Book
+from models.customer import Customer
+
+db_commands = Blueprint('db', __name__)
+
+@db_commands.cli.command('create')
+def create_all():
+    db.create_all()
+    print("Tables created")
+
+@db_commands.cli.command('drop')
+def drop_db():
+    db.drop_all()
+    print("Tables dropped")
+
+@db_commands.cli.command('seed')
+def seed_db():
+    customers = [
+        Customer(
+            name = 'Admin Joe',
+            email='admin@example.com',
+            password=bcrypt.generate_password_hash('eggs').decode('utf-8'),
+            # password = 'eggs',
+            address = '',
+            phone = '',
+            is_admin=True
+        ),
+        Customer(
+            name='John Smith',
+            email='user@example.com',
+            password=bcrypt.generate_password_hash('bacon').decode('utf-8'),
+            # password = 'eggs',
+            address = '123 road St, Suburb, QLD',
+            phone = '100 200 300 1',
+        )
+    ]
+
+    # Add the object as a new row to the table
+    db.session.add_all(customers)
+    db.session.commit()
+
+    books = [
+        Book(
+            title = 'Lord of the Rings: Fellowship of the Ring',
+            author = 'J. R. R. Tolkien',
+            description = 'They\'re taking the hobbits to Isengard',
+            category = 'fantasy'
+        ),
+        Book(
+            title = 'Lord of the Rings: The Two Towers',
+            author = 'J. R. R. Tolkien',
+            description = 'They\'re taking the hobbits to Isengard',
+            category = 'fantasy'
+        ),
+        Book(
+            title = 'Lord of the Rings: Return of the King',
+            author = 'J. R. R. Tolkien',
+            description = 'They\'re taking the hobbits to Isengard',
+            category = 'fantasy'
+        ),
+        Book(
+            title = 'Sense and Sensibility',
+            author = 'Jane Austin',
+            description = 'ye olde jaunt',
+            category = 'romance'
+        ),
+        Book(
+            title = 'Twilight',
+            author = 'Stephenie Meyer',
+            description = 'Hold on tight spider monkey',
+            category = 'romance'
+        ),
+        Book(
+            title = 'Misery',
+            author = 'Stephen King',
+            description = 'crazy lady break ankles',
+            category = 'horror'
+        ),
+        Book(
+            title = 'The Call of Cthulhu',
+            author = 'H. P. Lovecraft',
+            description = 'calamari boi',
+            category = 'horror'
+        )
+    ]
+    
+    db.session.add_all(books)
+    db.session.commit()
+    print("Table seeded")
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from models.customer import Customer, CustomerSchema
 from models.book import Book, BookSchema
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from controllers.cli_controller import db_commands
 import os
 
 app = Flask(__name__)
@@ -17,89 +18,4 @@ ma.init_app(app)
 bcrypt.init_app(app)
 jwt.init_app(app)
 
-
-
-
-# Define a custom CLI (terminal) command. Run with "flask create" in terminal.
-@app.cli.command('create')
-def create_all():
-    db.create_all()
-    print("Tables created")
-
-@app.cli.command("drop")
-def drop_db():
-    db.drop_all()
-    print("Tables dropped")
-
-@app.cli.command('seed')
-def seed_db():
-    customers = [
-        Customer(
-            name = 'Admin Joe',
-            email='admin@example.com',
-            # password=bcrypt.generate_password_hash('eggs').decode('utf-8'),
-            password = 'eggs',
-            address = '',
-            phone = '',
-            is_admin=True
-        ),
-        Customer(
-            name='John Smith',
-            email='user@example.com',
-            # password=bcrypt.generate_password_hash('bacon').decode('utf-8'),
-            password = 'eggs',
-            address = '123 road St, Suburb, QLD',
-            phone = '100 200 300 1',
-        )
-    ]
-
-    books = [
-        Book(
-            title = 'Lord of the Rings: Fellowship of the Ring',
-            author = 'J. R. R. Tolkien',
-            description = 'They\'re taking the hobbits to Isengard',
-            category = 'fantasy'
-        ),
-        Book(
-            title = 'Lord of the Rings: The Two Towers',
-            author = 'J. R. R. Tolkien',
-            description = 'They\'re taking the hobbits to Isengard',
-            category = 'fantasy'
-        ),
-        Book(
-            title = 'Lord of the Rings: Return of the King',
-            author = 'J. R. R. Tolkien',
-            description = 'They\'re taking the hobbits to Isengard',
-            category = 'fantasy'
-        ),
-        Book(
-            title = 'Sense and Sensibility',
-            author = 'Jane Austin',
-            description = 'ye olde jaunt',
-            category = 'romance'
-        ),
-        Book(
-            title = 'Twilight',
-            author = 'Stephenie Meyer',
-            description = 'Hold on tight spider monkey',
-            category = 'romance'
-        ),
-        Book(
-            title = 'Misery',
-            author = 'Stephen King',
-            description = 'crazy lady break ankles',
-            category = 'horror'
-        ),
-        Book(
-            title = 'The Call of Cthulhu',
-            author = 'H. P. Lovecraft',
-            description = 'calamari boi',
-            category = 'horror'
-        )
-    ]
-    # Add the object as a new row to the table
-    db.session.add_all(books)
-    db.session.add_all(customers)
-    # commit the changes
-    db.session.commit()
-    print("Table seeded")
+app.register_blueprint(db_commands)
