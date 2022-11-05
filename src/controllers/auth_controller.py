8a72e145c87fadb284b7ddaeff7678c5c29e885b
SheaDuffy13 <13420@coderacademy.edu.au>
2022-11-05 18:07:54 +1000

    added user registration

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -12,3 +12,20 @@ def get_users():
     stmt = db.select(User)
     users = db.session.scalars(stmt)
     return UserSchema(many=True, exclude=['password']).dump(users)    
+
+@auth_bp.route('/register/', methods=['POST'])
+def auth_register():
+    try:
+        # Create a new User model instance from the user_info
+        user = User(
+            email = request.json['email'],
+            password = bcrypt.generate_password_hash(request.json['password']).decode('utf8'),
+            name = request.json.get('name')
+        )
+        # Add and commit user to DB
+        db.session.add(user)
+        db.session.commit()
+        # Respond to client
+        return UserSchema(exclude=['password']).dump(user), 201
+    except IntegrityError:
+        return {'error': 'Email address already in use'}, 409
